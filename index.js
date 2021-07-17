dc502bb7c98438773668bba7fbb5cf2e168c9fc2
Kim Litjens <Kimlitjens4@gmail.com>
2021-07-17 20:50:00 +0200

    show color from user input

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const hexInput = document.getElementById('hexInput');
+const inputColor = document.getElementById('inputColor');
+
+hexInput.addEventListener('keyup', () => {
+    const hex = hexInput.value;
+    if (!isValidHex(hex)) return;
+
+    const strippedHex = hex.replace('#', '');
+
+    inputColor.style.backgroundColor = "#" + strippedHex;
+})
+
 const isValidHex = (hex) => {
     if (!hex) return false;
 
